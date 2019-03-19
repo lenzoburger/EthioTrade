0d116fd52058e6cef6d613129098de6bc3332457
Lencho Burka <lencho.burka@asb.co.nz>
2019-03-19 20:23:16 +1300

    add users model

---
+++ EthCoffee.api/Models/Users.cs
@@ -0,0 +1,13 @@
+namespace EthCoffee.api.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public byte [] PasswordHash { get; set; }
+
+        public byte [] PasswordSalt { get; set; }
+    }
+}

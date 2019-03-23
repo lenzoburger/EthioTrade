db6c63de0f0793449d83cb0edd9d5725b78c885b
Lencho Burka <lencho.burka@asb.co.nz>
2019-03-23 18:26:46 +1300

    AuthController - Login & Token Generator

--- EthCoffee.api/Controllers/AuthController.cs
+++ EthCoffee.api/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using EthCoffee.api.Data;
 using EthCoffee.api.Dtos;
 using EthCoffee.api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EthCoffee.api.Controllers
 {
@@ -11,13 +17,15 @@ namespace EthCoffee.api.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
 
-        public AuthController (IAuthRepository repo)
+        public AuthController (IAuthRepository repo, IConfiguration config)
         {
             _repo = repo;
+            _config = config;
         }
 
-         [HttpPost("register")]
+        [HttpPost("register")]
         public async Task<IActionResult> Register(UserRegisterDto UserRegDto)
         {
             UserRegDto.Username = UserRegDto.Username.ToLower();
@@ -31,6 +39,54 @@ namespace EthCoffee.api.Controllers
             var createdUser = await _repo.Register(userToCreate,UserRegDto.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginDto userLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userLoginDto.Username,userLoginDto.Password);
+
+            if (userFromRepo == null) 
+            {
+                return Unauthorized();
+            }
+            
+            var tokenDescriptor = CreateTokenDescriptor(userFromRepo);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok( new {
+                token = tokenHandler.WriteToken(token)
+            });
+        }
+
+        private SecurityTokenDescriptor CreateTokenDescriptor(User userFromRepo)
+        {
+            //Set claim/tokenSubject with username and ID
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            //Get and convert secret Key from AppSettings
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+            .GetBytes(_config.GetSection("AppSettings:Token").Value));
+            
+            //Hash Secret Jey
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            //Combine tokenSubject + expiryDate + Hashed secretKey
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            return tokenDescriptor;            
         }
     }
 }

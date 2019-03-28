7c1381d309575e5bee9ce6b376b9d2eaa0335c9c
Lencho Burka <lencho.burka@asb.co.nz>
2019-03-29 06:50:13 +1300

    update registration angular components

--- EthCoffee-SPA/src/app/register/register.component.ts
+++ EthCoffee-SPA/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,8 +6,9 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  model: any = {};
   @Input() valuesFromHome: any;
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
 
   constructor() { }
 
@@ -19,6 +20,7 @@ export class RegisterComponent implements OnInit {
   }
 
   cancel() {
+    this.cancelRegister.emit(false);
     console.log('cancelled');
   }
 

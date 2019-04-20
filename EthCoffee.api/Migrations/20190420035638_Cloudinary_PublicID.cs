8d61d2bada942dade72e148e29c18ad407fd7502
Lencho Burka <lencho.burka@asb.co.nz>
2019-04-20 16:02:08 +1200

    add Photo cloud storage solution; Cloudinary

---
+++ EthCoffee.api/Migrations/20190420035638_Cloudinary_PublicID.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EthCoffee.api.Migrations
+{
+    public partial class Cloudinary_PublicID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "ListingPhotos",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "Avatars",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "ListingPhotos");
+
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Avatars");
+        }
+    }
+}

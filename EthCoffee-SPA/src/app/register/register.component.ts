3dbe886c690e5d7601359ed5680c1f16cafbf466
Lencho Burka <lencho.burka@asb.co.nz>
2019-04-26 20:55:08 +1200

    Expand registration form & add reactive validations

--- EthCoffee-SPA/src/app/register/register.component.ts
+++ EthCoffee-SPA/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -13,18 +13,35 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   registerForm: FormGroup;
 
-  constructor(private authservice: AuthService, private alertify: AlertifyService) { }
+  constructor(private authservice: AuthService, private alertify: AlertifyService, private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.registerForm = new FormGroup({
-      username: new FormControl('Test', Validators.required),
-      password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]),
-      confirmPassword: new FormControl('', Validators.required)
-    }, this.passwordMatchValidator);
+    this.createRegistrationForm();
+  }
+
+  createRegistrationForm() {
+    this.registerForm = this.fb.group(
+      {
+        email: ['', Validators.required],
+        username: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]],
+        confirmPassword: ['', Validators.required],
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        dateOfBirth: ['', Validators.required],
+        gender: 'male',
+        phoneNumber: '',
+        country: '',
+        streetName: '',
+        city: ['', Validators.required],
+        region: ['Pick a Region...', Validators.required],
+        zipCode: ''
+      },
+      { validators: this.passwordMatchValidator });
   }
 
   passwordMatchValidator(form: FormGroup) {
-    return form.get('password').value === form.get('confirmPassword').value ? null : {mismatch : true};
+    return form.get('password').value === form.get('confirmPassword').value ? null : { mismatch: true };
   }
 
   register() {

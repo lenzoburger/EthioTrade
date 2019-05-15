a499b8a4afbbca8fabbea70dbae1acf26f1b5ed1
Lencho Burka <lencho.burka@asb.co.nz>
2019-05-15 20:57:36 +1200

    Update registration form validation, implement lastActive Date

--- EthCoffee-SPA/src/app/app.module.ts
+++ EthCoffee-SPA/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule } from 'ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -54,7 +55,8 @@ export function tokenGetter() {
     ListingEditComponent,
     MyAccountComponent,
     MyAccountEditComponent,
-    ListingPhotoEditorComponent
+    ListingPhotoEditorComponent,
+    TimeAgoPipe
   ],
   imports: [
     BrowserModule,

51d3c218e94acf6c0f3c108f6129850fb3027a7f
lenzoburger <lenzoburger@gmail.com>
2019-12-01 10:38:49 +1000

    fix: splice undefined error + MessageThreads scroll to Bottom & refresh functionality

--- EthCoffee-SPA/src/app/messagesContainer/message-thread/message-thread.component.ts
+++ EthCoffee-SPA/src/app/messagesContainer/message-thread/message-thread.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { Message, UserInfo } from "src/app/_models/message";
-import { MessageService } from "src/app/_services/message.service";
-import { AlertifyService } from "src/app/_services/alertify.service";
-import { AuthService } from "src/app/_services/auth.service";
-import { tap } from "rxjs/operators";
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
+import { Message, UserInfo } from 'src/app/_models/message';
+import { MessageService } from 'src/app/_services/message.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { AuthService } from 'src/app/_services/auth.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
-  selector: "app-message-thread",
-  templateUrl: "./message-thread.component.html",
-  styleUrls: ["./message-thread.component.css"]
+  selector: 'app-message-thread',
+  templateUrl: './message-thread.component.html',
+  styleUrls: ['./message-thread.component.css']
 })
-export class MessageThreadComponent implements OnInit {
+export class MessageThreadComponent implements OnInit, AfterViewChecked {
+  @ViewChild('messagesContainer', { static: true }) private messagesContainer: ElementRef;
+
   recipient: UserInfo;
   @Input()
   set messageRecipient(Recipient: UserInfo) {
@@ -19,14 +21,32 @@ export class MessageThreadComponent implements OnInit {
   }
   messages: Message[];
   newMessage: any = {};
+  prevMessagesLength = 0;
 
   constructor(
     private messageService: MessageService,
     private alertify: AlertifyService,
     private authservice: AuthService
-  ) {}
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.scrollToBottom();
+  }
+
+  ngAfterViewChecked() {
+    if (this.messages && this.prevMessagesLength !== this.messages.length) {
+      this.scrollToBottom();
+      this.prevMessagesLength = this.messages.length;
+    }
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.messagesContainer.nativeElement.scrollTop = this.messagesContainer.nativeElement.scrollHeight;
+    } catch (err) {
+      this.alertify.error(err);
+    }
+  }
 
   loadMessages() {
     const currentUserId = +this.authservice.decodedToken.nameid;
@@ -44,6 +64,7 @@ export class MessageThreadComponent implements OnInit {
       )
       .subscribe(
         messages => {
+          this.prevMessagesLength = this.messages ? this.messages.length : 0;
           this.messages = messages;
         },
         error => {
@@ -57,8 +78,9 @@ export class MessageThreadComponent implements OnInit {
     this.messageService
       .sendMessage(this.newMessage)
       .subscribe((message: Message) => {
+        this.prevMessagesLength = this.messages ? this.messages.length : 0;
         this.messages.unshift(message);
-        this.newMessage.content = "";
+        this.newMessage.content = '';
       });
   }
 }
